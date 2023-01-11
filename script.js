f05751c08d01854d30384dd2efd72d9ba74bfa68
ArokiaSamy45 <dossarokia45@gmail.com>
2023-01-11 20:21:04 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Question No. 2
+let resume = {
+    "Name" : "Arokia Samy",
+    "Age" : "26",
+    "Gender" : "Male",
+    "Education" : "B.Tech(Civil Engineering)",
+    "Contact Number" : "8015415456",
+    "Career Objective" : "To achieve a challenging career and I am keen in contributing effectively for the growth of the organisation.",
+
+}
+
+// Question No. 1 (for in loop)
+
+for (let i in resume) {
+
+    console.log(i, resume[i]);
+}
+
